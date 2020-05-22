02cd791cb3cd3c6dae8584f614e96a9c395548f4
Tomasz <tom.jopek@gmail.com>
2020-05-23 00:48:14 +0200

    appolo module added plus graphql api config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../shared/shared.module';
 import { FlexModule } from '@angular/flex-layout';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FlexModule } from '@angular/flex-layout';
         AppRoutingModule,
         BrowserAnimationsModule,
         SharedModule,
-        FlexModule
+        FlexModule,
+        GraphQLModule,
+        HttpClientModule
     ],
   providers: [],
   bootstrap: [AppComponent]
